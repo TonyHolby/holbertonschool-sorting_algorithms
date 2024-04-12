83a2fd1d38172beaf832a0eac7f3ac19f3c368da
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-12 13:35:43 +0000

    Fix code with the condition where the array is already sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,19 +10,36 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, k;
-	int temp;
+	int temp, swap_elements;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swap_elements = 0;
+
 		for (j = 0; j < size - i - 1; j++)
 		{
+			/**
+			 * if two integers are switched, swap_elements takes
+			 * the value 1.
+			 */
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = temp;
+				swap_elements = 1;
 			}
 		}
+
+		/**
+		 * if no integers are switched, the array is already sort and
+		 * the algorithme is stopped.
+		 */
+		if (swap_elements == 0)
+		{
+			break;
+		}
+
 		for (k = 0; k < size; k++)
 		{
 			printf("%d", array[k]);
@@ -31,6 +48,7 @@ void bubble_sort(int *array, size_t size)
 				printf(", ");
 			}
 		}
-		printf("\n");
 	}
+
+	printf("\n");
 }
