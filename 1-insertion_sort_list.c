ea0bd8ed57354cd22b5c83bf29ff7179e72a896f
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-12 12:15:55 +0000

    Add all the files from Tony's branch to the main branch

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list of
+ * integers in ascending order using the Insertion sort algorithm.
+ * @list: a doubly linked list from an array of integers.
+ */
+
+void insertion_sort_list(listint_t **list)
