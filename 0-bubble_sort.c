e087d55b45b4e9e0b884c6ed53c8f26a4642802c
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-12 10:28:25 +0000

    Fix code in the loop to print the evolution of sorting the array in 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,9 @@ void bubble_sort(int *array, size_t size)
 {
 	int temp;
 
-	for (size_t i = 0; i < size - 1; ++i)
+	for (size_t i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < size - i - 1; ++j)
+		for (size_t j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -22,5 +22,14 @@ void bubble_sort(int *array, size_t size)
 				array[j] = temp;
 			}
 		}
+		for (size_t k = 0; k < size; k++)
+		{
+			printf("%d", array[k]);
+			if (k < size - 1)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
 	}
 }
