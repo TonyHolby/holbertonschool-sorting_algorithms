c48e04dc4d223655a5641f2ffdf46b23acdf40b5
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-13 13:27:18 +0000

    Write the function insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,3 +7,46 @@
  */
 
 void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp, *prev_node, *next_node;
+
+	if (*list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		temp = current;
+
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			prev_node = temp->prev;
+			next_node = temp->next;
+			if (prev_node->prev != NULL)
+			{
+				prev_node->prev->next = next_node;
+			}
+
+			temp->prev = prev_node->prev;
+			temp->next = prev_node;
+			prev_node->prev = temp;
+			prev_node->next = next_node;
+
+			if (next_node != NULL)
+			{
+				next_node->prev = prev_node;
+			}
+
+			if (temp->prev == NULL)
+			{
+				*list = temp;
+			}
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
+
