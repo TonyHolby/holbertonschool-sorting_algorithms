52c65549e2c702b307c5de43f61c877935e5a0ca
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-14 19:59:23 +0000

    Fix Betty coding style in 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,25 +11,25 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
 	(*n1)->next = n2->next;
 
-        if (n2->next != NULL)
-        {
-                n2->next->prev = *n1;
-        }
+	if (n2->next != NULL)
+	{
+		n2->next->prev = *n1;
+	}
 
-        n2->prev = (*n1)->prev;
-        n2->next = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
 
-        if ((*n1)->prev != NULL)
-        {
-                (*n1)->prev->next = n2;
-        }
-        else
-        {
-                *h = n2;
-        }
+	if ((*n1)->prev != NULL)
+	{
+		(*n1)->prev->next = n2;
+	}
+	else
+	{
+		*h = n2;
+	}
 
-        (*n1)->prev = n2;
-        *n1 = n2->prev;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
@@ -44,13 +44,13 @@ void insertion_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	
+
 	/* Move through the list from 2nd node until end */
 	for (iteration = (*list)->next; iteration != NULL; iteration = temp)
 	{
 		temp = iteration->next;
 		insertion = iteration->prev;
-		
+
 		/* Match correct position of iteration in list */
 		while (insertion != NULL && iteration->n < insertion->n)
 		{
