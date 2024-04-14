17db734efbce1e650cbd7ed96bd0bccd03fdb213
PhantomWatchdog <159033827+PhantomWatchdog@users.noreply.github.com>
2024-04-14 20:01:11 +0200

    Optimize the insertion code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,52 +1,29 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - a function that sorts a doubly linked list of
+ * insertion_sort_list - Function sorting a doubly linked list of
  * integers in ascending order using the Insertion sort algorithm.
- * @list: a doubly linked list from an array of integers.
+ * @list: Doubly linked list from an array of integers.
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp, *prev_node, *next_node;
+	listint_t *iteration, *insertion, *temp;
 
-	if (*list == NULL || (*list)->next == NULL)
-	{
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	}
-
-	current = (*list)->next;
-
-	while (current != NULL)
+	
+	/* Move through the list from 2nd node until end */
+	for (iteration = (*list)->next; iteration != NULL; iteration = temp)
 	{
-		temp = current;
-
-		while (temp->prev != NULL && temp->n < temp->prev->n)
+		temp = iteration->next;
+		insertion = iteration->prev;
+		
+		/* Match correct position of iteration in list */
+		while (insertion != NULL && iteration->n < insertion->n)
 		{
-			prev_node = temp->prev;
-			next_node = temp->next;
-			if (prev_node->prev != NULL)
-			{
-				prev_node->prev->next = next_node;
-			}
-
-			temp->prev = prev_node->prev;
-			temp->next = prev_node;
-			prev_node->prev = temp;
-			prev_node->next = next_node;
-
-			if (next_node != NULL)
-			{
-				next_node->prev = prev_node;
-			}
-
-			if (temp->prev == NULL)
-			{
-				*list = temp;
-			}
-			print_list(*list);
+			swap_nodes(list, &insertion, iteration);
+			print_list((const listint_t *)*list);
 		}
-		current = current->next;
 	}
 }
-
