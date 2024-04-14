f6e67b0bc1eb558bcfe994b4c0bda20b4e3cc340
PhantomWatchdog <159033827+PhantomWatchdog@users.noreply.github.com>
2024-04-14 20:44:06 +0200

    Add function for swap nodes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,38 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - Swap two nodes in listint_t doubly-linked list.
+ * @h: Pointer to head of doubly-linked list.
+ * @n1: Pointer to first node to swap.
+ * @n2: Second node to swap.
+ */
+
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+
+        if (n2->next != NULL)
+        {
+                n2->next->prev = *n1;
+        }
+
+        n2->prev = (*n1)->prev;
+        n2->next = *n1;
+
+        if ((*n1)->prev != NULL)
+        {
+                (*n1)->prev->next = n2;
+        }
+        else
+        {
+                *h = n2;
+        }
+
+        (*n1)->prev = n2;
+        *n1 = n2->prev;
+}
+
+/**
  * insertion_sort_list - Function sorting a doubly linked list of
  * integers in ascending order using the Insertion sort algorithm.
  * @list: Doubly linked list from an array of integers.
